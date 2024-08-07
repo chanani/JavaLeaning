61580ef19bafeaa22b38ef85c13b62913d211f04
CHANHAN <130114269+chanani@users.noreply.github.com>
2024-08-07 22:22:50 +0900

    체크 예외 재정의

---
+++ java-high1/src/thread/control/CheckedExceptionMain.java
@@ -0,0 +1,19 @@
+package thread.control;
+
+public class CheckedExceptionMain {
+
+    public static void main(String[] args) throws Exception {
+        throw new Exception();
+    }
+
+    static class CheckedRunnable implements Runnable {
+
+        // 예외를 못던짐, 자바 문법 규칙과 맞지 않음
+        // 부모 메서드가 체크 예외를 던지지 않는 경우, 재정의된 자식 메서드도 체크 예외를 던질 수 없다.
+        @Override
+        public void run() /*throws Exception*/ {
+            // throw new Exception();
+        }
+    }
+
+}
