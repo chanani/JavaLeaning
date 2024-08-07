483dc31c1f6543cd7b2f1e650035bad0e497dc35
CHANHAN <130114269+chanani@users.noreply.github.com>
2024-08-07 22:14:19 +0900

    Thread의 생명 주기 실습

---
+++ java-high1/src/thread/control/ThreadInfoMain.java
@@ -0,0 +1,34 @@
+package thread.control;
+
+import thread.start.HelloRunnable;
+
+import static util.MyLogger.log;
+
+public class ThreadInfoMain {
+
+    public static void main(String[] args) {
+        // main 스레드
+        Thread mainThread = Thread.currentThread();
+        log("mainThread = " + mainThread);
+        //log("mainThread.threadId() = " + mainThread.threadId());
+        log("mainThread.threadId() = " + mainThread.getId());
+        log("mainThread.getName() = " + mainThread.getName());
+        // 우선 순위 기본이 5, 높을 수록 많이 실행됨
+        log("mainThread.getPriority() = " + mainThread.getPriority());
+        log("mainThread.getThreadGroup() = " + mainThread.getThreadGroup());
+        // 실행 중이거나 실행될 준비가 된 상타 RUNNABLE, 시작 전일 경우 NEW 등 블로그 참고
+        log("mainThread.getState() = " + mainThread.getState());
+
+        // myThread의 부모 Thread는 main Thread
+        // myThread 는 mainThread를 통해 생성되었기 때문
+        // 그룹은 부모 Thread의 그룹
+        Thread myThread = new Thread(new HelloRunnable(), "myThread");
+        log("myThread = " + myThread);
+        log("myThread.threadId() = " + myThread.getId());
+        log("myThread.getName() = " + myThread.getName());
+        log("myThread.getPriority() = " + myThread.getPriority());
+        log("myThread.getThreadGroup() = " + myThread.getThreadGroup());
+        log("myThread.getState() = " + myThread.getState());
+
+    }
+}
---
+++ java-high1/src/thread/start/InnerRunnableMainV3.java
@@ -0,0 +1,24 @@
+package thread.start;
+
+import static util.MyLogger.log;
+
+// 익명 클래스로 변수 만들지 않고 Runnable 만들기
+public class InnerRunnableMainV3 {
+
+    public static void main(String[] args) {
+        log("main() start");
+
+        // 단축키 option + command + n
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                log("run()");
+            }
+        });
+        thread.start();
+
+        log("main() end");
+    }
+
+
+}
