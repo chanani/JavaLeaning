1717eb46e21bfd39c30b2e2d1eee3b7ba4aed32f
CHANHAN <130114269+chanani@users.noreply.github.com>
2024-09-11 20:56:52 +0900

    [generic] 와일드카드 실습1

---
+++ generic/src/generic/ex5/WildcardMain1.java
@@ -0,0 +1,29 @@
+package generic.ex5;
+
+import generic.animal.Animal;
+import generic.animal.Cat;
+import generic.animal.Dog;
+
+public class WildcardMain1 {
+
+    public static void main(String[] args) {
+        Box<Object> objBox = new Box<>();
+        Box<Dog> dogBox = new Box<Dog>();
+        Box<Cat> catBox = new Box<Cat>();
+
+        dogBox.set(new Dog("멍멍이", 100));
+
+        WildcardEx.printGenericV1(dogBox);
+
+        WildcardEx.printWildcardV1(dogBox);
+
+        WildcardEx.printWildcardV2(dogBox);
+
+        WildcardEx.printGenericV2(dogBox);
+
+
+        Dog dog = WildcardEx.printANdReturnGeneric(dogBox);
+        Animal animal = WildcardEx.printANdReturnWildcard(dogBox);
+
+    }
+}
