484bcad93690d36fcd09bdd6b6e2a0b3cfd9f29b
CHANHAN <130114269+chanani@users.noreply.github.com>
2025-03-13 20:06:06 +0900

    [stream] 스트림의 기본 예제

--- generic/src/Main.java
+++ generic/src/Main.java
@@ -1,5 +1,14 @@
+import java.math.BigDecimal;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+
+        System.out.println(2.0 - 1.1);
+        System.out.println(2.0 - 1.11);
+        System.out.println(2.0 - 1.111);
+
+
     }
 }
