46a220fa293361eea84d457652ebb05cf63b9a99
CHANHAN <130114269+chanani@users.noreply.github.com>
2025-04-10 21:30:53 +0900

    [Lamda] 람다가 필요한 이유1

---
+++ java-adv3/src/lamda/start/Ex0RefMain.java
@@ -0,0 +1,17 @@
+package lamda.start;
+
+public class Ex0RefMain {
+
+    public static void main(String[] args) {
+        programMethode("Hello Java");
+        programMethode("Hello spring");
+    }
+
+    public static void programMethode(String content){
+        System.out.println("프로그램 시작");
+        System.out.println(content);
+        System.out.println("프로그램 종료");
+    }
+
+
+}
---
+++ stream/src/streamapi/StreamApiExample4.java
@@ -0,0 +1,16 @@
+package streamapi;
+
+
+/**
+ * Stream API 고급 활용
+ */
+public class StreamApiExample4 {
+
+    public static void main(String[] args) {
+        /**
+         * 참고 블로그 : https://mangkyu.tistory.com/115
+         */
+
+
+    }
+}
