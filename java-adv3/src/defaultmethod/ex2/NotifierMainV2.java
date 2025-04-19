e5deefd667d68f9b48099772c1f6670bd1c94e2e
CHANHAN <130114269+chanani@users.noreply.github.com>
2025-04-19 14:33:42 +0900

    [Default Method] 디폴트 메서드가 등장한 이유

---
+++ java-adv3/src/defaultmethod/ex2/NotifierMainV2.java
@@ -0,0 +1,22 @@
+package defaultmethod.ex2;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class NotifierMainV2 {
+
+    public static void main(String[] args) {
+        List<Notifier> notifiers = List.of(
+                new EmailNotifier(),
+                new SMSNotifier(),
+                new AppPushNotifier()
+        );
+        notifiers.forEach(n -> n.notify("서비스 가입을 환영합니다."));
+
+        // 스케출링 기능
+        LocalDateTime plusDays = LocalDateTime.now().plusDays(1);
+        notifiers.forEach(n ->
+                n.scheduleNotification("hello !", plusDays)
+        );
+    }
+}
